4729f670e22c60f40b4dd0c65ad093f2b341f815
Showmick Das <showmickdas75@gmail.com>
2025-07-22 05:37:43 +0600

    New initial format

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,21 @@
+import createMDX from "@next/mdx";
+import rehypePrism from "rehype-prism-plus";
+import remarkToc from "remark-toc";
+import rehypeSlug from "rehype-slug";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-}
+  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+};
 
-export default nextConfig
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+  options: {
+    remarkPlugins: [
+      [remarkToc, { tight: true, maxDepth: 3 }]
+    ],
+    rehypePlugins: [rehypePrism, rehypeSlug],
+  },
+});
+
+export default withMDX(nextConfig);
